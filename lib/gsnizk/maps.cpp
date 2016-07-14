3f7e7dadcba8b190f999f1fed914c05e0eb8642c
baz1 <bazin.remi@gmail.com>
2016-07-14 17:17:14 +0100

    Efficiency improvement (precomputation for scalar commitments)

--- lib/gsnizk/maps.cpp
+++ lib/gsnizk/maps.cpp
@@ -30,7 +30,6 @@ CRS::CRS(bool binding) : v1(G1(), G1::getRand()), v2(G2(), G2::getRand()),
     i1(Fp::getRand()), j1(Fp::getRand()),
     i2(Fp::getRand()), j2(Fp::getRand())
 {
-    // TODO precomputations?
     computeElements();
 }
 
@@ -60,6 +59,16 @@ std::istream &operator>>(std::istream &stream, CRS &crs) {
     if (crs.type == CRS_TYPE_PUBLIC) {
         stream >> crs.u1 >> crs.v1 >> crs.w1;
         stream >> crs.u2 >> crs.v2 >> crs.w2;
+#if !defined(USE_PBC)
+        /* Precomputations of generators in G1 and G2 */
+        crs.v1._2.precomputeForMult();
+        crs.v2._2.precomputeForMult();
+        /* Precomputations for commitments of scalars */
+        crs.u1._1.precomputeForMult();
+        crs.u1._2.precomputeForMult();
+        crs.u2._1.precomputeForMult();
+        crs.u2._2.precomputeForMult();
+#endif
     } else {
         stream >> crs.v1._2 >> crs.v2._2;
         stream >> crs.i1 >> crs.j1 >> crs.i2 >> crs.j2;
@@ -69,11 +78,14 @@ std::istream &operator>>(std::istream &stream, CRS &crs) {
 }
 
 void CRS::computeElements() {
+#if !defined(USE_PBC)
+    /* Precomputations of generators in G1 and G2 */
     v1._2.precomputeForMult();
+    v2._2.precomputeForMult();
+#endif
     v1._1 = j1 * v1._2;
     w1._1 = (i1 * j1) * v1._2;
     u1._1 = w1._1;
-    v2._2.precomputeForMult();
     v2._1 = j2 * v2._2;
     w2._1 = (i2 * j2) * v2._2;
     u2._1 = w2._1;
@@ -88,6 +100,13 @@ void CRS::computeElements() {
         u2._2 = i2 * v2._2;
         w2._2 = w2._2 - v2._2;
     }
+#if !defined(USE_PBC)
+    /* Precomputations for commitments of scalars */
+    crs.u1._1.precomputeForMult();
+    crs.u1._2.precomputeForMult();
+    crs.u2._1.precomputeForMult();
+    crs.u2._2.precomputeForMult();
+#endif
 }
 
 }
