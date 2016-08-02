c801038a0c4853aa47bb0e009450b5a7f5bd5fd7
baz1 <bazin.remi@gmail.com>
2016-08-02 20:00:54 +0100

    Structure updated

--- lib/gsnizk/gsnizk.h
+++ lib/gsnizk/gsnizk.h
@@ -38,27 +38,94 @@ enum ElementType {
  * @brief A @f$\mathbb{F}_p@f$ element inside an equation.
  */
 class FpElement {
-    friend class FpVar;
+    inline FpElement(const FpElement &other);
+    inline FpElement &operator=(const FpElement &other);
+    inline ~FpElement();
+private:
+    inline FpElement(ElementType type);
+public:
+    /**
+     * @brief Creates a variable in @f$\mathbb{F}_p@f$ with index @a index.
+     * @param index Index of the variable in @f$\mathbb{F}_p@f$.
+     * @return The variable element.
+     */
+    inline friend FpElement FpVar(int index);
+    /**
+     * @brief Creates a constant in @f$\mathbb{F}_p@f$ with index @a index.
+     * @param index Index of the constant in @f$\mathbb{F}_p@f$.
+     * @return The constant element.
+     */
+    inline friend FpElement FpConst(int index);
+    /**
+     * @brief Creates a constant in @f$\mathbb{F}_p@f$ with a specific value.
+     * @param value Value of the constant in @f$\mathbb{F}_p@f$.
+     * @return The constant element.
+     */
+    inline friend FpElement FpConst(Fp value);
 private:
     ElementType type;
     union {
         int index;
         Fp el;
-    } data;
+    };
 };
-
-/**
- * @brief Creates a variable in @f$\mathbb{F}_p@f$ with index @a index.
- * @param index Index of the variable in @f$\mathbb{F}_p@f$.
- * @return The variable element.
- */
-inline FpElement FpVar(int index);
 
 /**
  * @todo Continue and finish the proof system.
  */
 
 /* Inline definitions: */
+
+inline FpElement::FpElement(const FpElement &other) : type(other.type) {
+    if (type == ELEMENT_CONST_VALUE)
+        new (&el) Fp(other.el);
+    else
+        index = other.index;
+}
+
+inline FpElement &FpElement::operator=(const FpElement &other) {
+    if (type == ELEMENT_CONST_VALUE) {
+        if (other.type == ELEMENT_CONST_VALUE) {
+            el = other.el;
+        } else {
+            el.~Fp();
+            index = other.index;
+        }
+    } else {
+        if (other.type == ELEMENT_CONST_VALUE) {
+            new (&el) Fp(other.el);
+        } else {
+            index = other.index;
+        }
+    }
+    type = other.type;
+    return *this;
+}
+
+inline FpElement::~FpElement() {
+    if (type == ELEMENT_CONST_VALUE)
+        el.~Fp();
+}
+
+inline FpElement::FpElement(ElementType type) : type(type) {}
+
+inline FpElement FpVar(int index) {
+    FpElement el(ELEMENT_VARIABLE);
+    el.index = index;
+    return el;
+}
+
+inline FpElement FpConst(int index) {
+    FpElement el(ELEMENT_CONST_INDEX);
+    el.index = index;
+    return el;
+}
+
+inline FpElement FpConst(Fp value) {
+    FpElement el(ELEMENT_CONST_VALUE);
+    new (&el.el) Fp(value);
+    return el;
+}
 
 } /* End of gsnizk */
 
