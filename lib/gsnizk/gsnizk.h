5bca9a48445cd30d130c0a3d89f767607b113bb3
baz1 <bazin.remi@gmail.com>
2016-08-02 19:37:23 +0100

    Structure changed for Fp, G1, G2 and GT elements

--- lib/gsnizk/gsnizk.h
+++ lib/gsnizk/gsnizk.h
@@ -23,186 +23,42 @@
 namespace gsnizk {
 
 /**
+ * @cond INTERNAL_DATA_STRUCT
+ */
+enum ElementType {
+    ELEMENT_VARIABLE,
+    ELEMENT_CONST_INDEX,
+    ELEMENT_CONST_VALUE
+};
+/**
+ * @endcond
+ */
+
+/**
  * @brief A @f$\mathbb{F}_p@f$ element inside an equation.
  */
 class FpElement {
-};
-
-/**
- * @brief A variable in @f$\mathbb{F}_p@f$.
- */
-class FpVar : public FpElement {
-    /**
-     * @brief Creates a variable with index @a index.
-     * @param index Index of the variable in @f$\mathbb{F}_p@f$.
-     */
-    inline FpVar(int index);
+    friend class FpVar;
 private:
-    int index;
+    ElementType type;
+    union {
+        int index;
+        Fp el;
+    } data;
 };
 
 /**
- * @brief A constant in @f$\mathbb{F}_p@f$.
+ * @brief Creates a variable in @f$\mathbb{F}_p@f$ with index @a index.
+ * @param index Index of the variable in @f$\mathbb{F}_p@f$.
+ * @return The variable element.
  */
-class FpConst : public FpElement {
-    /**
-     * @brief Creates a constant with index @a index.
-     * @param index Index of the constant in @f$\mathbb{F}_p@f$.
-     */
-    inline FpConst(int index);
-    /**
-     * @brief Creates a constant with value @a value.
-     * @param value Value of the constant in @f$\mathbb{F}_p@f$.
-     */
-    inline FpConst(Fp value);
-private:
-    int index;
-    Fp el;
-};
-
-/**
- * @brief A @f$\mathbb{G}_1@f$ element inside an equation.
- */
-class G1Element {
-};
-
-/**
- * @brief A variable in @f$\mathbb{G}_1@f$.
- */
-class G1Var : public G1Element {
-    /**
-     * @brief Creates a variable with index @a index.
-     * @param index Index of the variable in @f$\mathbb{G}_1@f$.
-     */
-    inline G1Var(int index);
-private:
-    int index;
-};
-
-/**
- * @brief A constant in @f$\mathbb{G}_1@f$.
- */
-class G1Const : public G1Element {
-    /**
-     * @brief Creates a constant with index @a index.
-     * @param index Index of the constant in @f$\mathbb{G}_1@f$.
-     */
-    inline G1Const(int index);
-    /**
-     * @brief Creates a constant with value @a value.
-     * @param value Value of the constant in @f$\mathbb{G}_1@f$.
-     */
-    inline G1Const(G1 value);
-private:
-    int index;
-    G1 el;
-};
-
-/**
- * @brief A @f$\mathbb{G}_2@f$ element inside an equation.
- */
-class G2Element {
-};
-
-/**
- * @brief A variable in @f$\mathbb{G}_2@f$.
- */
-class G2Var : public G2Element {
-    /**
-     * @brief Creates a variable with index @a index.
-     * @param index Index of the variable in @f$\mathbb{G}_2@f$.
-     */
-    inline G2Var(int index);
-private:
-    int index;
-};
-
-/**
- * @brief A constant in @f$\mathbb{G}_2@f$.
- */
-class G2Const : public G2Element {
-    /**
-     * @brief Creates a constant with index @a index.
-     * @param index Index of the constant in @f$\mathbb{G}_2@f$.
-     */
-    inline G2Const(int index);
-    /**
-     * @brief Creates a constant with value @a value.
-     * @param value Value of the constant in @f$\mathbb{G}_2@f$.
-     */
-    inline G2Const(G2 value);
-private:
-    int index;
-    G2 el;
-};
-
-/**
- * @brief A @f$\mathbb{G}_T@f$ element inside an equation.
- */
-class GTElement {
-};
-
-/**
- * @brief A variable in @f$\mathbb{G}_T@f$.
- */
-class GTVar : public GTElement {
-    /**
-     * @brief Creates a variable with index @a index.
-     * @param index Index of the variable in @f$\mathbb{G}_T@f$.
-     */
-    inline GTVar(int index);
-private:
-    int index;
-};
-
-/**
- * @brief A constant in @f$\mathbb{G}_T@f$.
- */
-class GTConst : public GTElement {
-    /**
-     * @brief Creates a constant with index @a index.
-     * @param index Index of the constant in @f$\mathbb{G}_T@f$.
-     */
-    inline GTConst(int index);
-    /**
-     * @brief Creates a constant with value @a value.
-     * @param value Value of the constant in @f$\mathbb{G}_T@f$.
-     */
-    inline GTConst(GT value);
-private:
-    int index;
-    GT el;
-};
+inline FpElement FpVar(int index);
 
 /**
  * @todo Continue and finish the proof system.
  */
 
 /* Inline definitions: */
-
-inline FpVar::FpVar(int index) : FpElement(), index(index) {}
-
-inline FpConst::FpConst(int index) : FpElement(), index(index), el() {}
-
-inline FpConst::FpConst(Fp value) : FpElement(), index(-1), el(value) {}
-
-inline G1Var::G1Var(int index) : G1Element(), index(index) {}
-
-inline G1Const::G1Const(int index) : G1Element(), index(index), el() {}
-
-inline G1Const::G1Const(G1 value) : G1Element(), index(-1), el(value) {}
-
-inline G2Var::G2Var(int index) : G2Element(), index(index) {}
-
-inline G2Const::G2Const(int index) : G2Element(), index(index), el() {}
-
-inline G2Const::G2Const(G2 value) : G2Element(), index(-1), el(value) {}
-
-inline GTVar::GTVar(int index) : GTElement(), index(index) {}
-
-inline GTConst::GTConst(int index) : GTElement(), index(index), el() {}
-
-inline GTConst::GTConst(GT value) : GTElement(), index(-1), el(value) {}
 
 } /* End of gsnizk */
 
